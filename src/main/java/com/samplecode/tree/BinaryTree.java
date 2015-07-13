42a34e8c9384fc93762fc5f26f30f0f884b6771e
jojo <chiranjeeb.nandy@yahoo.com>
2015-07-12 20:54:37 -0500

    added binary tree

---
+++ src/main/java/com/samplecode/tree/BinaryTree.java
@@ -0,0 +1,17 @@
+package com.samplecode.tree;
+
+public class BinaryTree
+{
+	public void createBinaryTree()
+	{
+		
+	}
+	
+	private static class TreeNode
+	{
+		public TreeNode leftChild;
+		public TreeNode rightChild;
+		public TreeNode parent;
+		public int value;
+	}
+}
